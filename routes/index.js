7e72e0229637aeb04445d2ad901dc38d7d4742c1
ericwang1902 <2248930513@qq.com>
2016-12-16 17:46:35 +0800

    修改

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,10 @@ var roleRoutes = require('./roleRoutes')
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 
+
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', isLogedIn,function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -55,19 +57,21 @@ router.use('/role', roleRoutes);
 
 
 
-//登录中间件
-function isLogedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    req.flash('error_msg', '您尚未登陆！');
-    res.redirect("/login");
-  }
 
-}
-
+//**********************************测试区***************************
 //自定义回调函数
-router.post('/login', function (req, res, next) {
+router.post('/login', passportAuth,function(req,res,next){
+  console.log('success!')
+  return res.json({
+      state: 200,
+        authresult: true,
+        info: "info"
+  })
+});
+
+
+//passport登录中间件
+function passportAuth (req, res, next) {
   //自调用匿名函数，将req，res，next直接传递给了匿名函数
   //如果不通过验证，user的值为false
   passport.authenticate('local', function (err, user, info) {
@@ -80,26 +84,26 @@ router.post('/login', function (req, res, next) {
         authresult: false,
         info: info
       }
-      return res.send(JSON.stringify(result));
+        return res.json(result)
     }
     //可以直接在这里写验证成功的返回
-    
-
-
     //用户登录
     req.logIn(user, function (err) {
       if (err) { return next(err); }
-      return res.json({
-        state: 200,
-        authresult: true,
-        info: info
-      });
+      console.log("验证成功")
+      return next();
     });
-
-
   })(req, res, next);
+}
 
-});
+//检测是否登录中间件
+function isLogedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', '您尚未登陆！');
+    res.redirect("/login");
+  }
 
-
+}
 module.exports = router;
